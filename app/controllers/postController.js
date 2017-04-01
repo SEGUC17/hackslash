f6daaa8b5dadf3301f2ced52d0b06730a97a27f5
mohamedAlmorsi <mohamed.almorsi.alshaer@gmail.com>
2017-04-02 01:31:03 +0200

    ViewDone

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -1,4 +1,43 @@
 let Post = require('../models/post.js');
+let User = require('../models/user.js');
+
+
+//
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/pets';
+
+var insertDocument = function(db, callback) {
+   db.collection('user').insertOne( {
+
+     'username': 'mohamed', 'email': 'mohamed1', 'password': 'mohamed123',
+     'firstName': 'mohamed', 'lastName':'alshaer','rate':0.0,'rates':[],'verified':false ,'ratedItems':[]
+
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the user collection.");
+    callback();
+  });
+};
+
+var insertDocument1 = function(db, callback) {
+   db.collection('post').insertOne( {
+
+     'ownerEmail':'mohamed1','type':1,'kind':'kalb','species':'baladi','price':152.30
+
+
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the post collection.");
+    callback();
+  });
+};
+
+
+
+//
 let postController = {
 
 
@@ -82,7 +121,118 @@ let postController = {
                 else
                 res.json({posts});
         })
+    },
+
+
+
+
+
+
+
+
+
+    // showing the posts without the contact info of the users
+    viewPostsOnly : function(req,res){
+
+        // insterting
+
+        MongoClient.connect(url, function(err, db) {
+          assert.equal(null, err);
+          insertDocument(db, function() {
+              db.close();
+          });
+        });
+
+
+      Post.find({},function(err,posts){
+        if(err)
+          res.send(err.message);
+          else if (posts.length ==0 )
+            res.send("No posts , Yet ");
+            else
+              res.json({posts});
+      })
+    },
+    // showing the posts alongside with the info of the useres made these posts
+    viewPostsAndInfo : function(req,res){
+
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        insertDocument(db, function() {
+            db.close();
+        });
+      });
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        insertDocument1(db, function() {
+            db.close();
+        });
+      });
+
+/*
+      var allPosts = [] ;
+      var usersEmails =[] ;
+      var usersInfo = [] ;*/
+      // getting the posts
+
+      var usersInfo=[];
+
+      Post.find({},function(err,allPosts){
+
+        if(err)
+          res.send(err.message);
+          else if (allPosts.length == 0)
+            res.send("No Posts , Yet");
+            else{
+        for(var i =0 ;i<allPosts.length;i++){
+        User.findOne({email : allPosts[i].ownerEmail},function(err,user){
+        /*  if(err){
+            res.send(err.message);
+          }
+          else {
+            usersInfo[i] =user;
+            console.log(usersInfo.length);
+          }*/
+          usersInfo[i] =user;
+
+        });
+
+        }
+          JSON.stringify(usersInfo);
+           res.json({allPosts,usersInfo});
+
+          }
+
+      })
+      /*
+      console.log(allPosts);
+
+      // getting the mails of the users that made posts
+      for(var i =0 ; i<allPosts.length ; i++){
+        usersEmails[i]=allPosts[i].ownerEmail;
+
+      }
+      // getting the info of the useres that made posts
+      for(var i =0 ;i<usersEmails.length;i++){
+      usersInfo[i] = User.findOne({email : usersEmails[i]});
+      }
+
+      res.json({allPosts,usersInfo});*/
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = postController;
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-//Telling our project that we're using the express framwork
+//Telling our project that we're using the express framework
 var express = require('express');
 var router = express.Router();
 var postController = require('./controllers/postController');
@@ -9,5 +9,7 @@ var postController = require('./controllers/postController');
 router.get('/post/search', postController.searchPosts);
 router.get('/post/filter', postController.filterPosts);
 router.get('/post/searchAndfilter', postController.searchAndFilterPosts);
+router.get('/post',postController.viewPostsOnly);
+router.get('/postSign',postController.viewPostsAndInfo);
 //Export router.
 module.exports = router;
