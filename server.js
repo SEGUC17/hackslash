50cbbb478d454ca61a869428ecb1ddc3b84b5239
MohamedMeguid <mohamedhussein96@gmail.com>
2017-04-03 21:46:54 +0200

    getting ready to merge

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var nev = require('email-verification')(mongoose);
-// var path = require('path');
-
-// var User = require('./app/models/user');
 
 // REQUIRING CONFIG FILE
 var config = require('./app/config/config.js');
@@ -32,13 +29,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 // REQUIRING ROUTES
-var routes = require('./app/routes/routes.js');
 var registerRoutes = require('./app/routes/register.js');
 
-app.use(routes);
+// USING ROUTES
 app.use(registerRoutes);
 
-//Starting the server on port 8080
+// Starting the server on port 8080
 app.listen(8080, function()
 {
     console.log('Server started in 8080');
