391215b9186eed0786172593913769c05c842e7e
Mahmoud Salem <memo0o0salem@gmail.com>
2017-04-05 12:18:01 +0200

    editing in the last merge

--- app/routes/client.js
+++ app/routes/client.js
@@ -6,21 +6,14 @@ var middleware = require("../middleware");
 var postController = require('../controllers/postController.js');
 
 
-//=======
 
-
-//>>>>>>> origin/master
 router.get('/post/viewPostsAndInfo',middleware.isLoggedIn,postController.viewPostsAndInfo);
 router.post('/post/review',middleware.isLoggedIn,postController.reviewPost);
 router.post('/post/exchange',middleware.isLoggedIn , postController.Exchange_Post);
 router.post('/post/edit',middleware.isLoggedIn, postController.edit_post);
-
-//////////////////////////////////////////////////////////////////
-
 router.post('/post/shelter',middleware.isLoggedIn, postController.Shelter_Post);
 router.post('/post/lost',middleware.isLoggedIn, postController.lost_Post);
 router.post('/post/found',middleware.isLoggedIn, postController.Found_Post);
-
 
 
 
