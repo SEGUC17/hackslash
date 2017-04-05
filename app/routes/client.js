286513d7a503a8ed7732ec8041d85ebcde24cf92
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2017-04-05 02:29:09 +0200

    fixing route bug

--- app/routes/client.js
+++ app/routes/client.js
@@ -7,8 +7,8 @@ var postController = require('../controllers/postController.js');
 
 
 
-router.get('post/viewPostsAndInfo',middleware.isLoggedIn,postController.viewPostsAndInfo);
-router.post('post/review',middleware.isLoggedIn,postController.reviewPost);
+router.get('/post/viewPostsAndInfo',middleware.isLoggedIn,postController.viewPostsAndInfo);
+router.post('/post/review',middleware.isLoggedIn,postController.reviewPost);
 router.post('/post/exchange',middleware.isLoggedIn , postController.Exchange_Post);
 router.post('/post/edit',middleware.isLoggedIn, postController.edit_post);
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,7 @@ var postController = require('../controllers/postController.js');
 router.get('/post/search', postController.searchPosts);
 router.get('/post/filter', postController.filterPosts);
 router.get('/post/searchAndfilter', postController.searchAndFilterPosts);
-router.get('post/view',postController.viewPostsOnly);
+router.get('/post/view',postController.viewPostsOnly);
 
 
 module.exports = router;
