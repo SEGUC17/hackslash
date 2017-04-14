8e262f0196eb4ee438f511987705130719e11837
Mahmoud-Salem <memo0o0salem@gmail.com>
2017-04-14 12:58:55 +0200

    edit  service finished

--- public/js/services/postService.js
+++ public/js/services/postService.js
@@ -4,29 +4,32 @@ angular.module('postService', [])
     .factory('Post', function($http) {
         return {
             add : function(postData,type) {
-                return $http.get('/api/todos');
                 switch(type)
                 {
                     case "sell" :
                         return $http.post('/post/sell');
                         break ;
                     case "shelter":
-                        return $http.post('/post/sell');
+                        return $http.post('/post/shelter');
                         break ;
                     case "mate":
-                        return $http.post('/post/sell');
+                        return $http.post('/post/mate');
                         break ;
                     case "lost":
-                        return $http.post('/post/sell');
+                        return $http.post('/post/lost');
                         break ;
                     case "found":
-                        return $http.post('/post/sell');
+                        return $http.post('/post/found');
                         break ;
                     case "exchange":
-                        return $http.post('/post/sell');
+                        return $http.post('/post/exchange');
                         break ;
 
                 }
+            }
+            ,edit :function(postData) {
+                return $http.post('/post/edit');
+                
             }
         }
     });
