2b207d8453c50c28453a46141c720c20d75ce6b6
MohamedMeguid <mohamedhussein96@gmail.com>
2017-04-14 20:38:40 +0200

    posts are showing at last el7

--- public/js/controllers/postsController.js
+++ public/js/controllers/postsController.js
@@ -2,13 +2,40 @@ angular.module('pettts')
 
   .controller('postsController',function($scope, postsService){
 
-     //$scope.hao = "hoahoa";
-     $scope.posts = JSON.stringify(postsService.get());
-     $scope.hao = postsService.hao;
+    postsService.get().then(function(posts){
+      $scope.posts = posts
+    });
+     //$scope.posts = postsService.get();
+    console.log($scope.posts);
 
-      if(!$scope.posts)
-  		  $scope.notFound = true;
-  	  else
-  		  $scope.notFound = false;
+    if(!$scope.posts)
+      $scope.notFound = true;
+    else
+      $scope.notFound = false;
 
 });
+
+// custom filter
+angular.module('pettts')
+
+  // switch numbers stored in the database to strings to display
+  .filter('stringifyType', function(){
+    return function(x){
+      switch (x) {
+        case 1:
+          return "Buy a pet";
+        case 2:
+          return "Shelter a pet";
+        case 3:
+          return "Get your pet a date";
+        case 4:
+          return "A pet is Lost";
+        case 5:
+          return "A pet is found";
+        case 6:
+          return "Exchange pets"
+        default:
+          return "";
+      }
+    }
+  });
--- public/js/services/postsService.js
+++ public/js/services/postsService.js
@@ -3,10 +3,37 @@ angular.module('pettts')
   .factory('postsService', ['$http', function($http) {
 
     return {
-    // get all posts
-          get: function() {
-              return $http.get('/post/view');
-          },
-          hao: "hao hao"
+      // get all posts
+      get: function() {
+          return $http.get('/post/view')
+            .then(function successCallback(res){
+              console.log(res.data.posts);
+              return res.data.posts;
+            });
+      }
     }
+
+    // return {
+    // // get all posts
+    //       get: function() {
+    //           return $http.get('/post/view');
+    //       }
+    // }
+
+    // this.posts = [];
+    //
+    // $http({
+    //   method: 'GET',
+    //   url: '/post/view'
+    // }).then(function successCallback(response) {
+    //     this.posts = response.data.posts
+    //   }, function errorCallback(response) {
+    //     console.log(response);
+    //   });
+    //
+    // console.log(this.posts);
+    //
+    // this.getPosts = function(){
+    //   return this.posts;
+    // }
 }]);
