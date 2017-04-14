ffca0e4622c1d12fe9d071d72f59380f22776341
Mahmoud-Salem <memo0o0salem@gmail.com>
2017-04-14 13:34:44 +0200

    edit controller

--- public/js/controllers/postController.js
+++ public/js/controllers/postController.js
@@ -2,7 +2,7 @@ angular.module('postController', [])
 
     .controller('postController', function($scope, $http, Post) {
         $scope.formData = {};
-
+        var type = "";
         // addPost ===========================
         $scope.addPost = function() {
             Post.add($scope.formData)
@@ -12,6 +12,38 @@ angular.module('postController', [])
         };
         
         // editPost ==========================
-        
+        $scope.editPost = function() {
+            switch(formData.type)
+            {
+                case 1 :  
+                    type = "sell";
+                    break ;
+                case 2 :  
+                    type = "shelter";
+                    break ;
+                case 3 :  
+                    type = "mate";
+                    break ;
+                case 4 :  
+                    type = "lost";
+                    break ;
+                case 5 :  
+                    type = "found";
+                    break ;  
+                case 6 :  
+                    type = "exchange";
+                    break ;   
+                default :
+                    type = "error";
+                    break ;
+            }
+            if(type != "error")
+            {
+            Post.edit($scope.formData,type)
+                .success(function() {
+                     $scope.formData = {}; // clear the form so our user is ready to enter another
+                });
 
+                }; 
+             }
     });
