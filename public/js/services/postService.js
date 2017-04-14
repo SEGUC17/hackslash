f459b820f109340f13f3f3cfa3b4738c70369666
Mahmoud-Salem <memo0o0salem@gmail.com>
2017-04-14 13:05:49 +0200

    adding data with request

--- public/js/services/postService.js
+++ public/js/services/postService.js
@@ -7,28 +7,28 @@ angular.module('postService', [])
                 switch(type)
                 {
                     case "sell" :
-                        return $http.post('/post/sell');
+                        return $http.post('/post/sell',postData);
                         break ;
                     case "shelter":
-                        return $http.post('/post/shelter');
+                        return $http.post('/post/shelter',postData);
                         break ;
                     case "mate":
-                        return $http.post('/post/mate');
+                        return $http.post('/post/mate',postData);
                         break ;
                     case "lost":
-                        return $http.post('/post/lost');
+                        return $http.post('/post/lost',postData);
                         break ;
                     case "found":
-                        return $http.post('/post/found');
+                        return $http.post('/post/found',postData);
                         break ;
                     case "exchange":
-                        return $http.post('/post/exchange');
+                        return $http.post('/post/exchange',postData);
                         break ;
 
                 }
             }
             ,edit :function(postData) {
-                return $http.post('/post/edit');
+                return $http.post('/post/edit',postData);
                 
             }
         }
