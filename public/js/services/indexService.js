56b250cfc4ae6ccfdac5834e4a2d2de7e6403aef
Mohamed Abdelmeguid <mohamedhussein96@gmail.com>
2017-04-15 21:22:04 +0200

    Deleted unneeded indexService file

--- public/js/services/indexService.js
+++
@@ -1,8 +0,0 @@
-angular.module('pettts').service('index', ['$http', function($http) {
-
-  // get all posts
-        this.get = function() {
-            return $http.get('/post/view');
-        }
-        this.hao = "hao";
-}]);
