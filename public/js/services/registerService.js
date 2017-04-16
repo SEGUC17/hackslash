cb19e64ba834422708690eec57f4111b87200efa
MohamedKhaledAbdelhamid <mohamedkhaled_51908@hotmail.com>
2017-04-16 02:47:27 +0200

    updated registerService.js

--- public/js/services/registerService.js
+++ public/js/services/registerService.js
@@ -1,1 +1,5 @@
-angular.module('registerService', [])
+var app = angular.module('registerService', []);
+
+    app.factory('User', function($http) {
+      return $http.post('/register', user);
+    })
