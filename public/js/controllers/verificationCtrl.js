c21c82fea66443dd52a047e8e3741022db8c3d96
mohamed <mohamed_ahmed_95@live.com>
2017-04-23 14:07:44 +0200

    documenting the controllers

--- public/js/controllers/verificationCtrl.js
+++ public/js/controllers/verificationCtrl.js
@@ -1,10 +1,13 @@
 var app = angular.module('pettts');
 
 app.controller('verificationController', function($scope, $http, verificationService, $routeParams) {
+
     $scope.message = undefined;
     $scope.verificationToken = $routeParams.verification;
-    verificationService.get($scope.verificationToken).then(function(response){
-        console.log(response);
+    //send the verifiation token to the service to make the request
+    verificationService.get($scope.verificationToken).then(function(response) {
+        //set the result to the scope
         $scope.message = response.message;
+
     })
 });
