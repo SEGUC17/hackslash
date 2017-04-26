3fedd053f749cf074b66f5eaf26f056d795f98b8
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2017-04-26 22:48:56 +0200

    added check to phone number in register controller

--- public/js/controllers/registerCtrl.js
+++ public/js/controllers/registerCtrl.js
@@ -10,14 +10,34 @@ app.controller('registerCtrl', function($scope, $http, $window, User) {
 
     ////////////////////////////////////
     ///////////////////////////////////
+    
     // add user to the family
-
     $scope.submitUser = function() {
-
+        //Check if phone number(s) entered have valid characters (+12345... or (12)345... or 12345... or 12-345...)
+        $scope.message = undefined;
+        //Regular expression that contains '+', '-', '(', ')', ' ' and numbers.
+        var regex = /^[\d ()+-]+$/;
         var user = $scope.user;
+        if($scope.user.phoneNumber1){
+            if(!regex.test($scope.user.phoneNumber1)){
+                $scope.message = "Invalid phone number in field Phone number 1. It should only contain numbers, '+', '(', ')' and '-'.";
+                return;
+            }
+        }
+        if($scope.user.phoneNumber2){
+            if(!regex.test($scope.user.phoneNumber2)){
+                $scope.message = "Invalid phone number in field Phone number 2. It should only contain numbers, '+', '(', ')' and '-'.";
+                return;
+            }
+        }
+        if($scope.user.homeNumber){
+            if(!regex.test($scope.user.homeNumber)){
+                $scope.message = "Invalid phone number in field Home number. It should only contain numbers, '+', '(', ')' and '-'.";
+                return;
+            }
+        }
         //call the service function
         User.add(user, $scope);
-
     };
 });
 
