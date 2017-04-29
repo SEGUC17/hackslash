04326fd4a5a1767be58630a40883623a590d1f28
mohamed <mohamed_ahmed_95@live.com>
2017-04-29 20:33:16 +0200

    #71 fix type of verification message

--- public/js/controllers/registerCtrl.js
+++ public/js/controllers/registerCtrl.js
@@ -10,58 +10,67 @@ app.controller('registerCtrl', function($scope, $http, $window, User) {
 
     ////////////////////////////////////
     ///////////////////////////////////
-    
+
     // add user to the family
     $scope.submitUser = function() {
         //Check if phone number(s) entered have valid characters (+12345... or (12)345... or 12345... or 12-345...)
         $scope.message = undefined;
+        $scope.error = undefined;
         //Regular expression that contains '+', '-', '(', ')', ' ' and numbers.
         var regex = /^[\d ()+-]+$/;
         var user = $scope.user;
         //Check on Phone number 1 (if provided)
-        if($scope.user.phoneNumber1){
-            if(!regex.test($scope.user.phoneNumber1)){
+        if ($scope.user.phoneNumber1) {
+            if (!regex.test($scope.user.phoneNumber1)) {
+                $scope.error = true;
                 $scope.message = "Invalid phone number in field Phone number 1. It should only contain numbers, '+', '(', ')' and '-'.";
                 return;
             }
         }
         //Check on Phone number 2 (if provided)
-        if($scope.user.phoneNumber2){
-            if(!regex.test($scope.user.phoneNumber2)){
+        if ($scope.user.phoneNumber2) {
+            if (!regex.test($scope.user.phoneNumber2)) {
+                $scope.error = true;
                 $scope.message = "Invalid phone number in field Phone number 2. It should only contain numbers, '+', '(', ')' and '-'.";
                 return;
             }
         }
         //Check on Home number (if provided)
-        if($scope.user.homeNumber){
-            if(!regex.test($scope.user.homeNumber)){
+        if ($scope.user.homeNumber) {
+            if (!regex.test($scope.user.homeNumber)) {
+                $scope.error = true;
                 $scope.message = "Invalid phone number in field Home number. It should only contain numbers, '+', '(', ')' and '-'.";
                 return;
             }
         }
         //Check on username
-        if($scope.user.username.length > 30 || $scope.user.username.length < 5){
+        if ($scope.user.username.length > 30 || $scope.user.username.length < 5) {
+            $scope.error = true;
             $scope.message = "Invalid username. It must be between 5 and 30 charcters long.";
             return;
         }
         //Check on password
-        if($scope.user.password.length < 5){
+        if ($scope.user.password.length < 5) {
+            $scope.error = true;
             $scope.message = "Invalid password. It should be at least 5 charcters long.";
             return;
         }
         //Check on first name
-        if($scope.user.firstName.length > 30 || $scope.user.firstName.length < 1){
+        if ($scope.user.firstName.length > 30 || $scope.user.firstName.length < 1) {
+            $scope.error = true;
             $scope.message = "Invalid first name. It must be between 1 and 30 charcters long";
             return;
         }
         //Check on last name
-        if($scope.user.lastName.length > 30 || $scope.user.lastName.length < 1){
+        if ($scope.user.lastName.length > 30 || $scope.user.lastName.length < 1) {
+            $scope.error = true;
             $scope.message = "Invalid last name. It must be between 1 and 30 charcters long";
             return;
         }
         //Check on middle name (if provided)
-        if($scope.user.middleName){
-            if($scope.user.middleName.length > 30 || $scope.user.middleName.length < 1){
+        if ($scope.user.middleName) {
+            $scope.error = true;
+            if ($scope.user.middleName.length > 30 || $scope.user.middleName.length < 1) {
                 $scope.message = "Invalid middle name. It must be between 1 and 30 charcters long";
                 return;
             }
--- public/js/services/registerService.js
+++ public/js/services/registerService.js
@@ -2,12 +2,13 @@ var app = angular.module('pettts');
 
 app.factory('User', function($http, $window) {
 
-  // This Service is used to connect the reviewPostctrl to the API
+    // This Service is used to connect the reviewPostctrl to the API
 
     return {
-      // Add Function is used to add the user in the database of the server
-      // by sending the required information about the new user .
+        // Add Function is used to add the user in the database of the server
+        // by sending the required information about the new user .
         add: function(user, $scope) {
+            $scope.error = undefined;
             var fd = new FormData();
             for (var key in user)
                 fd.append(key, user[key]);
@@ -15,12 +16,15 @@ app.factory('User', function($http, $window) {
                 transformRequest: angular.indentity,
                 headers: { 'Content-Type': undefined }
             }).then(function successCallback(response) {
-              // verification mail has been sent Successfully .
+                // verification mail has been sent Successfully .
                 $scope.message = "verification mail sent";
+                $scope.error = undefined;
                 return response;
             }, function errorCallback(response) {
-              // An Error has occcured .
+                // An Error has occcured .
+                $scope.error = true;
                 $scope.message = response.data.message;
+
                 return response;
             });
         }
